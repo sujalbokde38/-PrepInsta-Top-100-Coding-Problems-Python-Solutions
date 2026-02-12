be7248560ce3f9d9e7fa7b14cc0409ca1f7996fe
sujal bokde <sujalbokde38@gmail.com>
2026-02-12 14:46:15 +0530

    Add comments to sumN function for better clarity and rewritten the logic logic
    Added inline comments to enhance understanding of the sumN function's logic and edge cases.

--- Getting Started/3.sum of first N natural numbers.py
+++ Getting Started/3.sum of first N natural numbers.py
@@ -1,13 +1,14 @@
-def sumN(n):
+def sumN(n): #defined a function named sum
     a = 0
-    if n <= 0:
+    if n <= 0: #edge case for negative number and zero
         print("Invalid input")
         return
 
-    for i in range(1, n + 1):
+    for i in range(1, n + 1): #logic
         a += i
 
     print(a)
 
 n = int(input("Enter a number: "))
 sumN(n)
+
